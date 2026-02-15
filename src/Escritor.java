39c4f216043c71fe049c1b53adbc22ddcfab4792
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:36:42 +0100

    Comentarios agregados

--- src/Escritor.java
+++ src/Escritor.java
@@ -4,6 +4,8 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class Escritor {
+    //Metodo guardarEmeplado que se le pasa una lista de empleados y una cadena 
+    //con la ruta y nombre del archivo , y crea el fichero binario 
     public void guardarEmpleado(List<Empleado> empleados, String ruta) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
             oos.writeObject(empleados);
